524f0942d5bbf3dc075b88d4bfde18d487b82ef2
Brandan Richardson <brandanr@gmail.com>
2018-03-01 22:00:24 -0800

    server.js is working, handlebars is not

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require('express-handlebars');
 var methodOverride = require('method-override');
 
 // Sets up the Express App
@@ -18,7 +19,11 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 // Static directory
 app.use(express.static("app/public"));
