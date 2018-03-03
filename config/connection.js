d35c519f0223184e95e92afdaf71892afef9efcd
Brandan Richardson <brandanr@gmail.com>
2018-03-02 23:37:52 -0800

    can post new burgers, but not switch from eat to devoured

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,21 @@
 var mysql = require("mysql");
 
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+if(process.env.JAWSDB_URL) {
+  // Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  // local host
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+};
 
 // Connect to the database
 connection.connect(function(err) {
